c0d9dba7ad428695ddd23a8ea2de6870a5960297
AlphaDelta1803 <ipad4dasika@gmail.com>
2021-11-19 17:03:29 +0530

    Updated PythagoreanTriplet.py
    Signed-off-by: AlphaDelta1803 <ipad4dasika@gmail.com>

--- lab_programs/week4/PythagoreanTriplet.py
+++ lab_programs/week4/PythagoreanTriplet.py
@@ -2,15 +2,18 @@
 # and print a message informing the user whether they are a pythagorean
 # triplet.
 a, b, c = input("Enter three numbers: ").split(sep=" ")
-if int(a) > int(b) and int(a) > int(c):
-    largest = int(a)
-    rhs = int(c) ** 2 + int(b) ** 2
+a = int(a)
+b = int(b)
+c = int(c)
+if a > b and a > c:
+    largest = a
+    rhs = c ** 2 + b ** 2
 elif b > c:
-    largest = int(b)
-    rhs = int(c) ** 2 + int(a) ** 2
+    largest = b
+    rhs = c ** 2 + a ** 2
 else:
-    largest = int(c)
-    rhs = int(a) ** 2 + int(b) ** 2
+    largest = c
+    rhs = a ** 2 + b ** 2
 if rhs == (largest ** 2):
     print("The numbers are a pythagorean triplet")
 else:
